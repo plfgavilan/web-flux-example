4d4447e8ce5cb61a8c3c3ba7a9b6391beeb848cd
plfgavilan <pedrocantoria@gmail.com>
2022-02-12 16:46:10 +0100

    Added 2 endpoints to get employees

--- src/main/java/com/example/webfluxexample/EmployeeController.java
+++ src/main/java/com/example/webfluxexample/EmployeeController.java
@@ -1,13 +1,28 @@
 package com.example.webfluxexample;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
 
-//    private final EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
-    // constructor...
+    @GetMapping("/{id}")
+    public Mono<Employee> getEmployeeById(@PathVariable String id) {
+        return employeeRepository.findById(id);
+    }
+
+    @GetMapping
+    public Flux<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
 }
