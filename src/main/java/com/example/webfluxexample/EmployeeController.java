c7583e972c1c9c66bb8414c9f6cd225e6b0c64b5
plfgavilan <pedrocantoria@gmail.com>
2022-02-12 16:34:09 +0100

    Initial commit

---
+++ src/main/java/com/example/webfluxexample/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+//    private final EmployeeRepository employeeRepository;
+
+    // constructor...
+}
---
+++ src/main/java/com/example/webfluxexample/WebFluxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxExampleApplication.class, args);
+	}
+
+}
